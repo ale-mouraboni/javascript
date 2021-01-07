f86b57059d29cee193673c2cf2404d548d3883f5
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-01-07 18:11:20 -0300

    var/let

---
+++ udemy/curso de javascript e typescript/aula6/index.js
@@ -0,0 +1,13 @@
+/*
+    não podemos criar variáveis com palavras reservadas
+    variáveis precisam ter nomes significativos
+    não pode começar o nome de uma variável com um número
+    não pode conter espaço ou traços
+    utilizamos camelCase
+    case-sensitive
+    não podemos redeclarar variáveis com let
+    NÃO UTILIZE VAR, UTILIZE LET.
+*/
+
+let nomeCliente = 'Luiz'
+nomeCliente = 'Otávio'

7b7db9a00ac6a7e7f304a955a32f404cf9d0df82
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-01-07 17:17:21 -0300

    comment js

---
+++ udemy/curso de javascript e typescript/aula4/comentarios.js
@@ -0,0 +1,11 @@
+// escrever um comentário
+console.log('Olá, Mundo!') // aqui tem outro comentário
+console.log('Linha 1')
+console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+// vai ser ignorado
+
+/*
+    comentário de bloco
+*/

32c108c6048a86017e56ae1072a9ddbf6f9f14c8
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-01-08 17:17:25 -0300

    new folders

---
+++ udemy/curso de javascript e typescript/aula7/index.js
@@ -0,0 +1,13 @@
+/*
+    não podemos criar constantes com palavras reservadas
+    constantes precisam ter nomes significativos
+    não pode começar o nome de uma constante com um número
+    não pode conter espaço ou traços
+    utilizamos camelCase
+    case-sensitive
+    não podemos modificar o valor de uma constante
+    NÃO UTILIZE VAR, UTILIZE CONST.
+*/
+
+const nome = 'João';
+console.log(nome)

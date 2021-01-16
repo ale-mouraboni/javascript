16b803294789ed81c21a82f365bff4142d2a0c67
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-01-15 21:32:09 -0300

    const

--- udemy/curso de javascript e typescript/aula7/index.js
+++ udemy/curso de javascript e typescript/aula7/index.js
@@ -9,5 +9,17 @@
     NÃO UTILIZE VAR, UTILIZE CONST.
 */
 
-const nome = 'João';
-console.log(nome)
+/*
+    string = text | number = número    
+*/
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(typeof resultado)
+
+

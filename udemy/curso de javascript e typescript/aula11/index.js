4227c61171e7f17e63371989d3a8275af2454f07
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-01-23 09:57:37 -0300

    operadores aritméticos

---
+++ udemy/curso de javascript e typescript/aula11/index.js
@@ -0,0 +1,35 @@
+/*
+ * aritméticos
+ * + adição / concatenação
+ * - subtração
+ * / divisão
+ * * multiplicação
+ * ** potenciação
+ * % resto da divisão
+ */
+
+/*
+ * precedência
+ * ()
+ * **
+ * * / %
+ * + -
+ */
+
+/*
+ * incremento = ++ , temos o pré incremento e o pós incremento
+ * decremento = -- , temos o pré decremento e o pós decremento
+ */
+
+/*
+ * operadores de atribuição
+ * **=
+ * +=
+ * -=
+ */
+
+// NaN - not a number, parseInt(number inteiro) parseFloat(number decimal)
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2);
+console.log(typeof num2);

3e4a088c6becd677fd1cfa4ac4465dd6808aea67
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-01-28 10:03:00 -0300

    correção exercício

--- udemy/curso de javascript e typescript/aula12/js/script.js
+++ udemy/curso de javascript e typescript/aula12/js/script.js
@@ -1,3 +1,4 @@
+/* meu exercício
 let num1 = prompt(`Digite um número:`);
 let num2 = prompt(`Digite outro número:`);
 
@@ -6,4 +7,14 @@ num2 = Number(num2);
 
 const resultado = num1 + num2
 
-alert(`O resultado é ${resultado}`)
+alert(`O resultado é: ${resultado}`)
+*/
+
+
+let num1 = prompt(`Digite um número:`);
+let num2 = prompt(`Digite outro número:`);
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+console.log(`O resultado é: ${num1 + num2}`) // também é possível exibir dessa forma, diminui o tamanho do código mas deixa mais complexo.

047f8f5d1f0b84650e49f6eb281fff1f7115dff7
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2021-02-17 09:30:26 -0300

    exercicio com string, correção professor

--- udemy/curso de javascript e typescript/aula13/aula13.js
+++ udemy/curso de javascript e typescript/aula13/aula13.js
@@ -13,8 +13,8 @@ console.log(string.match(/[a-z]/)) // referente a expressões regulares
 console.log(string.search(/[a-z]/)) // similar ao indexOf, mas ele aceita expressões regulares
 console.log(string.replace('Um', 'Outro')) // substitui letras e palavras (aceita expressões regulares)
 console.log(string.length) // mostra quantos caractéres tem na string (começa contando do 0 e conta os espaços)
-console.log(string.slice(-5)) //
+console.log(string.slice(-5)) // mostra os caractéres com base no valor atribuido
 console.log(string.substring(string.length -5)) // 
-console.log(string.split(' ')) // 
+console.log(string.split(' ')) // adiciona um espaço entre os caracteres
 console.log(string.toUpperCase()) // transforma os caractéres em maiusculos
 console.log(string.toLowerCase()) // transforma os caractéres em minusculos 
